673f2b6e0d6c24617cbd0180a2d236774e2fff7d
Akshay K <ak@m4.local>
2026-08-11 05:14:05 +0530

    update folder

--- logical_and.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-read -p "Enter your age: " age
-read -p "enter your country name: " country
-if [[ $age -ge 18 ]] && [[ $country == "India" ]]
-then echo "you can vote"
-else echo  "you cant vote"
-fi
